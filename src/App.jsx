2b0149e37638fbc2c368c0279daef73353ec0176
Kim-Tsok <appkim02@gmail.com>
2024-05-01 02:30:24 +0100

    More Changes were made to the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,11 @@ import Navbar from "./Navbar";
 function App() {
   return (
     <>
-      <Navbar />
+      <div className="bg-[#eadfd8] h-screen w-full">
+        <div className="flex items-center w-screen justify-center">
+          <Navbar />
+        </div>
+      </div>
     </>
   );
 }
